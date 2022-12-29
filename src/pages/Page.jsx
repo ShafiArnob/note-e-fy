83d2d6b51643538fc4454c1ba6b7b7fe9810de47
ShafiArnob <shafiarnob@gmail.com>
2022-12-29 06:56:14 +0600

    single page data load

--- src/pages/Page.jsx
+++ src/pages/Page.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 const Page = () => {
+
+  const [page, setPage] = useState({})
   const params = useParams()
+
+  useEffect(()=>{
+    fetch(`http://localhost:8000/pages/${params.id}`)
+    .then(res => res.json())
+    .then(data => setPage(data))
+  },[])
+  console.log(page);
   return (
-    <div>Page {params.id}</div>
+    <div>
+      {/* Title */}
+      <div className='mx-6 my-4'>
+        <h2 className='text-xl font-bold'>{page.title}</h2>
+      </div>
+
+      {/* kanban */}
+      <div>
+
+      </div>
+    </div>
   )
 }
 
