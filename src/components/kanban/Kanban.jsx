c862e84771d94fb8d0d1adee0848f93cca8419c7
ShafiArnob <shafiarnob@gmail.com>
2022-12-29 12:35:26 +0600

    update kanban tasks

--- src/components/kanban/Kanban.jsx
+++ src/components/kanban/Kanban.jsx
@@ -3,8 +3,41 @@ import { DragDropContext, Droppable } from 'react-beautiful-dnd'
 import KanbanCol from './KanbanCol'
 
 const Kanban = ({page}) => {
+  const updateTask = data =>{
+    fetch(`http://localhost:8000/pages/${page.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(res=>res.json())
+    .then(data=> data)
+  }
   const onDragEnd = result =>{
-    console.log(result);
+    // console.log(result);
+    if (!result.destination) return
+    const { source, destination } = result
+
+    if (source.droppableId !== destination.droppableId) {
+        const sourceColIndex = page.kanban.findIndex(e => e.id === source.droppableId)
+        const destinationColIndex = page.kanban.findIndex(e => e.id === destination.droppableId)
+        // console.log(sourceColIndex, destinationColIndex);
+        
+        const sourceCol = page.kanban[sourceColIndex]
+        const destinationCol = page.kanban[destinationColIndex]
+
+        const sourceTask = [...sourceCol.tasks]
+        const destinationTask = [...destinationCol.tasks]
+
+        const [removed] = sourceTask.splice(source.index, 1)
+        destinationTask.splice(destination.index, 0, removed)
+
+        page.kanban[sourceColIndex].tasks = sourceTask
+        page.kanban[destinationColIndex].tasks = destinationTask
+
+        updateTask(page)
+    }
   }
   return (
     <>
