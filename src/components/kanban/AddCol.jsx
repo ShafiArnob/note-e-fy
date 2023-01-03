175e03291d7d15ff3adb637e1a9cfd870865c2c8
ShafiArnob <shafiarnob@gmail.com>
2023-01-03 08:27:29 +0600

    add col bug fix

--- src/components/kanban/AddCol.jsx
+++ src/components/kanban/AddCol.jsx
@@ -5,7 +5,9 @@ import { updateTask } from './kanbanFunctions'
 const AddCol = ({page}) => {
   const [colTitle, setColTitle] = useState('')
   const tasks = []
-  const addColumn = () => {
+  const addColumn = (e) => {
+    e.preventDefault()
+    console.log("f");    
     const data = {
       id: uuidv4(),
       title: colTitle,
@@ -15,13 +17,14 @@ const AddCol = ({page}) => {
     page.kanban.push(data)
 
     updateTask(page)
+    setColTitle('')
   }
   // console.log(colTitle);
   return (
-    <div className=" flex flex-col justify-center cursor-pointer w-2/12 h-full bg-[#383838] p-2 rounded-lg ml-4 border-dashed border-2 border-green-600">
+    <form onSubmit={addColumn} className=" flex flex-col justify-center cursor-pointer w-2/12 h-full bg-[#383838] p-2 rounded-lg ml-4 border-dashed border-2 border-green-600">
       <input onChange={(e)=>setColTitle(e.target.value)} value={colTitle} type="text" name="" id="" className='p-2 m-1 rounded-md bg-[#212121]'/>
-      <p onClick={() => addColumn()} className='bg-green-500 hover:bg-green-600 text-neutral-100 font-semibold p-1 m-1 my-1.5 w-4/12 rounded-md text-center'>Add</p>
-    </div>
+      <button className='bg-green-500 hover:bg-green-600 text-neutral-100 font-semibold p-1 m-1 my-1.5 w-4/12 rounded-md text-center'>Add</button>
+    </form>
   )
 }
 
