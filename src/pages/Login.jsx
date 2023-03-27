37acfb73321132ba4f67eeaa0e13627af75e323c
ShafiArnob <shafiarnob@gmail.com>
2023-03-27 09:19:08 +0600

    ligin ui bug fix

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -43,8 +43,8 @@ const Login = () => {
     signInError = <p className=' p-2 py-4 rounded-lg border-4 border-red-700 bg-red-600 bg-opacity-60'><small>{error.message}</small></p>
   }
   return (
-    <div className='h-full flex flex-col items-center justify-center'>
-      <form onSubmit={onSubmit} className='inline-block bg-[#383838] p-9 w-1/4 rounded-lg flex flex-col  justify-center space-y-4'>
+    <div className='h-full flex flex-col items-center justify-center '>
+      <form onSubmit={onSubmit} className='flex flex-col  justify-center bg-[#383838] p-9 rounded-lg space-y-4'>
         <h1 className='text-center font-bold font-mono text-2xl'>Login</h1>
 
         <div>
